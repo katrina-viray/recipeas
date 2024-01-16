d85453397ff889778e97b8b2cea19c960d9ad811
Katrina <katvir3@gmail.com>
2024-01-16 15:04:07 -0600

    button update

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,10 +1,22 @@
+import React, {useState} from 'react';
+
 const Button = ({title}) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+      };
+      
     return (
         <div>
-            <button className="bg-white border-solid border hover:bg-main-blue
-                 hover:text-white hover:border-slate-500 hover:drop-shadow 
-                hover:outline-none font-bold py-1.5 w-36
-                  rounded-full">{title}</button>
+            <button 
+                className={`${isClicked ? 'bg-main-blue': 'bg-white'} ${isClicked ? 'text-white': 'text-black'}
+                ${isClicked ? 'border-slate-500': 'border'} ${isClicked ? 'drop-shadow': ''} ${isClicked ? 'border-none': ''} 
+                ${isClicked ? 'hover:bg-blue-500': 'hover:bg-gray-300'} hover:border
+                border-solid border font-bold py-1.5 w-36 rounded-full hover:drop-shadow`}
+                onClick={handleClick}
+                >{title}
+            </button>
         </div>
     )
 }

fa87c3828484ca91fb8d13204166e1b583f6ea21
doseka3 <katvir3@gmail.com>
2024-02-07 01:26:53 -0600

    added different colors to tags in recipe page

--- frontend/src/components/RecipeDetails.jsx
+++ frontend/src/components/RecipeDetails.jsx
@@ -30,19 +30,47 @@ const RecipeDetails = ({ recipe, handleUpdateRecipe }) => {
 
   const titlePadding = Math.max(0, 64 - recipe.title.length);
   const typePadding = Math.max(0, 36 - recipe.type.length);
+  let typeColor, fontColor;
+
+  switch(recipe.type) {
+    case "Breakfast":
+      typeColor="bg-breakfast-color"
+      fontColor="black"
+      break;
+    case "Lunch":
+      typeColor="bg-lunch-color"
+      fontColor="black"
+      break;
+    case "Dinner":
+      typeColor="bg-dinner-color"
+      fontColor="black"
+      break;
+    case "Dessert":
+      typeColor="bg-dessert-color"
+      fontColor="black"
+      break;
+    case "Snack":
+      typeColor="bg-snack-color"
+      fontColor="black"
+      break;
+    default:
+      typeColor="main-purple"
+      fontColor="white"
+  }
+
 
   return (
     <tr className="border-b">
       <td className="">
         <Link to={`/recipes/${recipe._id}`}>
-          <p className="pl-3">{recipe.title}</p>
+          <p className="pl-4">{recipe.title}</p>
         </Link>
       </td>
 
       <td className="py-1.5">
-        <p className="bg-main-blue py-0.5 px-4 rounded-3xl inline-block">
-          {recipe.type}
-        </p>
+          <p className={`${typeColor} text-${fontColor} py-0.5 px-4 rounded-3xl inline-block`}>
+            {recipe.type}
+          </p>
       </td>
 
       <td className="pl-6">

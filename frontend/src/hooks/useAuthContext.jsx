ffdc49cae8cc41030eea4429469328f6f5f72ade
Katrina <katvir3@gmail.com>
2024-02-18 23:21:27 -0600

    authorization context

---
+++ frontend/src/hooks/useAuthContext.jsx
@@ -0,0 +1,15 @@
+import { RecipesContext } from '../context/AuthContext'
+import { useContext } from 'react'
+
+export const useAuthContext = () => {
+  // passing in the object with the state and dispatch function
+  const context = useContext(AuthContext)
+
+  // if context is null, like if the ContextProvider is being used on a component
+  // which isn't part of what the ContextProvider is wrapping. (like outside of App)
+  if (!context){
+    throw Error('useAuthContext must be used inside a AuthContextProvider')
+  }
+
+  return context
+} 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {RecipesContextProvider} from './context/RecipeContext'
+import {AuthContextProvider} from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RecipesContextProvider>
-      <App />
-    </RecipesContextProvider>
+    <AuthContextProvider>
+      <RecipesContextProvider>
+        <App />
+      </RecipesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
